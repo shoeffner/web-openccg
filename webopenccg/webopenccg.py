c70627d25cd7c943fa8863e3d4c28a79fe9f42cf
Sebastian Höffner <info@sebastian-hoeffner.de>
2018-11-16 09:13:00 +0100

    Adding ?graphs handling and making graphs optional.

--- webopenccg/webopenccg.py
+++ webopenccg/webopenccg.py
@@ -5,7 +5,8 @@ from flask import (Blueprint,
                    Flask,
                    redirect,
                    render_template,
-                   request)
+                   request,
+                   url_for)
 
 import graphs
 import wccg
@@ -16,7 +17,7 @@ bp = Blueprint('openccg', __name__,
                static_folder='static')
 
 
-def create_response(sentence):
+def create_response(sentence, add_graphs=False):
     """Prepares a response.
 
     Parses the sentence using wccg, then enriches the result with
@@ -35,10 +36,11 @@ def create_response(sentence):
         'uuid': str(uuid.uuid4())
     }
     response.update(content)
-    try:
-        response.update(graphs.create_graphs(content['json_parses']))
-    except KeyError:
-        pass
+    if add_graphs:
+        try:
+            response.update(graphs.create_graphs(content['json_parses']))
+        except KeyError:
+            pass
 
     return response
 
@@ -56,9 +58,9 @@ def parse():
     if request.method == 'GET':
         return json.dumps(dict(error="Use POST.", http_status=501)), 501
 
-    if 'sentence' not in request.values:
+    if 'sentence' not in request.form:
         try:
-            key = next(request.values.keys())
+            key = next(request.form.keys())
         except StopIteration:
             key = None
     else:
@@ -68,7 +70,7 @@ def parse():
     # The first key could be send e.g. by
     #     curl --data "This is the sentence." 127.0.0.1:5000/parse
     sentence = request.form.get(key) or key
-    response = create_response(sentence)
+    response = create_response(sentence, 'graphs' in request.args)
 
     return json.dumps(response), response['http_status']
 
@@ -96,7 +98,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def redirect_to_blueprint():
-    return redirect('openccg', 301)
+    return redirect(url_for('openccg.index', **request.args), 308)
+
+
+@app.route('/parse', methods=['GET', 'POST'])
+def redirect_to_parse():
+    return redirect(url_for('openccg.parse', **request.args), 308)
 
 
 app.register_blueprint(bp, url_prefix='/openccg')
